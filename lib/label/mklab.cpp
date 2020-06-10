edc61bb1d3e089bc8de1920d32c12318b3f79bb9
ikiissel <ikiissel@gmail.com>
2020-06-10 09:47:22 +0300

    -

--- lib/label/mklab.cpp
+++ lib/label/mklab.cpp
@@ -762,7 +762,7 @@ CFSArray<CFSWString> do_all(CFSWString utt, bool print_label, bool print_utt) {
 	Printer P;
 	for (INTPTR i = 0; i < TU.TPA.GetSize(); i++) {
 		res.AddItem(TU.TPA[i].Phone);
-		P.prnn(TU.TPA[i].Phone);
+		//P.prnn(TU.TPA[i].Phone);
 
 	}
 	return res;
